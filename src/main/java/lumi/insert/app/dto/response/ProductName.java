59b57e8e9ddb4b0a6cf7968f783588828e1ac5aa
KelvinKhodes <kelvinkho050@gmail.com>
2026-08-24 10:52:57 +0700

    feat: add stockQuantity field

--- src/main/java/lumi/insert/app/dto/response/ProductName.java
+++ src/main/java/lumi/insert/app/dto/response/ProductName.java
@@ -3,6 +3,8 @@ package lumi.insert.app.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+import java.math.BigDecimal;
+
 @Builder
 @Schema(description = "Minimalist response containing only the identity and name of a product")
 public record ProductName (
@@ -11,7 +13,10 @@ public record ProductName (
     Long id, 
     
     @Schema(description = "Full display name of the product", example = "Oli Yamalube Super Sport 1L")
-    String name
+    String name,
+
+    @Schema(description = "The current number of units available in the warehouse/store", example = "42")
+    BigDecimal stockQuantity
     
 )  implements Identifiable {
 
