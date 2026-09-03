9f0b20d7c36238bea04d4a7923b4e7d50cf9f638
KelvinKhodes <kelvinkho050@gmail.com>
2026-09-03 15:49:48 +0700

    feat: adjust some styling

--- src/main/java/lumi/insert/app/utils/generator/PdfPageTemplate.java
+++ src/main/java/lumi/insert/app/utils/generator/PdfPageTemplate.java
@@ -1,20 +1,11 @@
 package lumi.insert.app.utils.generator;
 
-import org.openpdf.text.Document;
-import org.openpdf.text.DocumentException;
-import org.openpdf.text.Element;
-import org.openpdf.text.ExceptionConverter;
-import org.openpdf.text.Font;
-import org.openpdf.text.FontFactory;
-import org.openpdf.text.PageSize;
-import org.openpdf.text.Paragraph;
-import org.openpdf.text.Phrase;
-import org.openpdf.text.Rectangle;
-import org.openpdf.text.pdf.PdfContentByte;
-import org.openpdf.text.pdf.PdfPCell;
-import org.openpdf.text.pdf.PdfPTable;
-import org.openpdf.text.pdf.PdfPageEventHelper;
-import org.openpdf.text.pdf.PdfWriter;
+import org.openpdf.text.*;
+import org.openpdf.text.pdf.*;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Utilities of Pdf's related.
@@ -34,12 +25,19 @@ public class PdfPageTemplate extends PdfPageEventHelper{
             PdfPTable headerTable = new PdfPTable(2);
             headerTable.setWidthPercentage(100);
             headerTable.setWidths(new float[]{1.5f, 4.0f}); 
-            headerTable.setSpacingAfter(5f);  
-    
-            // Image logo = Image.getInstance("path/to/logo.png");
-            // logo.scaleToFit(80, 80); 
-            
-            PdfPCell logoCell = new PdfPCell(new Phrase("LOGO", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18)));
+            headerTable.setSpacingAfter(5f);
+
+            URL resource = getClass().getResource("/media/logo.png");
+
+            PdfPCell logoCell;
+            if(resource != null){
+                Image logo = Image.getInstance(resource);
+                logo.scaleToFit(50, 50);
+                logo.setBackgroundColor(ExtendedColor.LIGHT_GRAY);
+                logoCell = new PdfPCell(logo);
+            } else {
+                logoCell = new PdfPCell(new Phrase("LOGO", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18)));
+            }
             logoCell.setBorder(Rectangle.NO_BORDER);
             logoCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             headerTable.addCell(logoCell);
@@ -48,14 +46,14 @@ public class PdfPageTemplate extends PdfPageEventHelper{
             detailsCell.setBorder(Rectangle.NO_BORDER);
             detailsCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
 
-            Paragraph companyName = new Paragraph("Lumi Insert Inc.", 
+            Paragraph companyName = new Paragraph("Lumi Insert",
                     FontFactory.getFont(FontFactory.HELVETICA_BOLD, 14));
             companyName.setAlignment(Element.ALIGN_RIGHT);
             
-            Paragraph address = new Paragraph("Jl. Lumi No. 123, Kota Bandung\n" +
-                    "Email: support@erp-anda.com | Telp: (0778) 123456\n" +
-                    "Website: www.erp-anda.com", 
-                    FontFactory.getFont(FontFactory.HELVETICA, 10));
+            Paragraph address = new Paragraph("Jl. Batam Center No. 123, Kota Batam\n" +
+                    "Email: support@lumi-insert.my.id | Telp: (0778) 123456\n" +
+                    "Website: www.lumi-insert.my.id",
+                    FontFactory.getFont(FontFactory.HELVETICA, 10, ExtendedColor.LIGHT_GRAY));
             address.setAlignment(Element.ALIGN_RIGHT);
 
             detailsCell.addElement(companyName);
@@ -69,7 +67,7 @@ public class PdfPageTemplate extends PdfPageEventHelper{
             cb.moveTo(document.leftMargin(), writer.getVerticalPosition(false) - 5);
             cb.lineTo(document.getPageSize().getWidth() - document.rightMargin(), writer.getVerticalPosition(false) - 5);
             cb.stroke();
-        } catch (DocumentException de) {
+        } catch (DocumentException | IOException de) {
             throw new ExceptionConverter(de);
         }
         
